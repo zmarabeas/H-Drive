e9bf44e44124e8bc2b5c06206d9f75733d50e3c0
Zach Marabeas <zmarabeas@gmail.com>
2019-08-19 02:49:14 -0400

    works

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -6,6 +6,9 @@ Servo fl;
 Servo fr;
 Servo bl;
 Servo br;
+Servo lateral_l;
+Servo lateral_r;
+
 
 RF24 radio(9, 10);
 const byte data_pipe[6] = "00001";
@@ -21,14 +24,20 @@ void setup() {
   Serial.begin(9600);
   printf_begin();
 
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  lateral_l.attach(3);
+  lateral_r.attach(4);
   fl.attach(5);
   fr.attach(6);
   br.attach(7);
   bl.attach(8);
+  lateral_l.write(90);
+  lateral_r.write(90);
   fl.write(90);
   fr.write(90);
   bl.write(90);
@@ -70,36 +79,49 @@ void loop() {
       ly -= 90;
       rx -= 90;
 
+      // left = (ly + rx)/2
+      // right = (ly - rx)/2
+      // lateral = lx
+
       //fl = -ly - lx -rx
       //fr = ly - lx - rx
       //br = ly + lx - rx
       //bl = -ly + lx - rx
-      int drive_fl = -ly - lx - rx;
+      //left
+      int drive_fl = (ly + rx)/2;
       drive_fl = constrain(drive_fl, -90, 90);
-      int drive_fr = ly - lx - rx;
-      drive_fr = constrain(drive_fr, -90, 90);
-      int drive_br = ly + lx - rx;
-      drive_br = constrain(drive_br, -90, 90);
-      int drive_bl = -ly + lx - rx;
+      int drive_bl = (ly + rx)/2;
       drive_bl = constrain(drive_bl, -90, 90);
+      //right
+      int drive_fr = (ly - rx)/2;
+      drive_fr = constrain(drive_fr, -90, 90);
+      int drive_br = (ly - rx)/2;
+      drive_br = constrain(drive_br, -90, 90);
+      //lateral
+      int drive_lateral = lx;
 
       //make positive again
       drive_fl += 90;
       drive_fr += 90;
       drive_br += 90;
       drive_bl += 90;
+      drive_lateral += 90;
 
       //fl and fr, 0 is forward
       //br and bl, 180 is forward
       //invert fr and bl
       drive_fr = map(drive_fr, 0, 180, 180, 0);
-      drive_bl = map(drive_bl, 0, 180, 180, 0);
+      drive_fl = map(drive_fl, 0, 180, 180, 0);
+      int drive_inverted_lateral = map(drive_lateral, 0, 180, 180, 0);
 
       //30 and 160 is max power for some reason
       drive_fl = constrain(drive_fl, 30,160);
       drive_fr = constrain(drive_fr, 30,160);
       drive_bl = constrain(drive_bl, 30,160);
       drive_br = constrain(drive_br, 30,160);
+      drive_lateral = constrain(drive_lateral, 30, 160);
+      drive_inverted_lateral = constrain(drive_inverted_lateral, 30, 160);
+      
 
       printf("fl: %d, fr: %d, br: %d, bl: %d\n", drive_fl, drive_fr, drive_br, drive_bl);
       
@@ -107,6 +129,8 @@ void loop() {
       fr.write(drive_fr);
       bl.write(drive_bl);
       br.write(drive_br);
+      lateral_r.write(drive_lateral);
+      lateral_l.write(drive_inverted_lateral);
 
       
     }
